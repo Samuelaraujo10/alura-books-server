8894ff291a796c63f549b18fd162dbc3e18f220e
Samuel Araújo <samuelaraujo71@gmail.com>
2026-04-09 21:48:25 -0300

    Add favoritos route and controller for managing favorite books

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const rotaLivro = require('./routes/livro.js')
+const rotaFavorito = require('./routes/favorito.js')
+
 const cors = require('cors')
 
 const app = express()
@@ -7,8 +9,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({origin: "*"}))
 
-
 app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
 const port = 8000
 
